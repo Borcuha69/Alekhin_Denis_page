164052a5199596c9f460679feaa858cdc4b05a2c
Borcuha69 <borcuha69@gmail.com>
2017-12-20 16:50:13 +0300

    Commit 1.5

---
+++ orders/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from .models import *
+
+
+class ProductInOrderInline(admin.TabularInline):
+    model = ProductInOrder
+    extra = 0
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Status._meta.fields]
+
+    class Meta:
+        model = Status
+
+
+admin.site.register(Status, StatusAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer_name', 'customer_phone', 'total_price', 'status', 'created', 'updated']
+    search_fields = ['status', 'customer_name']
+    inlines = [ProductInOrderInline]
+
+    class Meta:
+        model = Order
+
+
+admin.site.register(Order, OrderAdmin)
+
+
+class ProductInOrderAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProductInOrder._meta.fields]
+    search_fields = ['product', 'order']
+
+    class Meta:
+        model = ProductInOrder
+
+
+admin.site.register(ProductInOrder, ProductInOrderAdmin)
